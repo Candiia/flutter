a86d6cd4481696cf89482c7d6498be3ee19a65ca
Candi Alcantarilla <alcantarilla.cacan24@triana.salesianos.edu>
2024-12-10 21:57:09 +0100

    ejercicio 2 terminado

--- ejercicio2/lib/list_profile.dart
+++ ejercicio2/lib/list_profile.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Listprofile extends StatelessWidget {
   const Listprofile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Padding(
+          const Padding(
             padding: EdgeInsets.only(top: 70, left: 30),
             child: Text(
               'Best Iranian Actor and Actresses',
@@ -19,20 +20,91 @@ class Listprofile extends StatelessWidget {
                   fontWeight: FontWeight.bold),
             ),
           ),
-          Padding(
+          const Padding(
             padding: EdgeInsets.only(left: 30),
             child: Text('March 2020',
                 style: TextStyle(
                     color: Color.fromARGB(95, 73, 72, 72), fontSize: 18)),
           ),
-          SizedBox(
-            height: 300,
+          Expanded(
             child: ListView(
-                    key: GlobalKey(),
-                    scrollDirection: Axis.horizontal, 
-                    padding: const EdgeInsets.all(8.0),
-                    children: [],
+              scrollDirection: Axis.horizontal,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 30, top: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(180),
+                        child: Image.network(
+                          'https://media.themoviedb.org/t/p/w300_and_h450_bestv2/5Qne374OM0ewMM7uSN9eq9jNrWq.jpg',
+                          height: 500,
+                          width: 300,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 40),
+                        child: Text(
+                          'Ana',
+                          style: GoogleFonts.montserrat(
+                              textStyle: const TextStyle(fontSize: 30)),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 40),
+                        child: Text(
+                          'De Armas',
+                          style: GoogleFonts.montserrat(
+                            textStyle: const TextStyle(
+                                fontSize: 18,
+                                color: Color.fromARGB(95, 73, 72, 72)),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30, top: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(180),
+                        child: Image.network(
+                          'https://media.themoviedb.org/t/p/w300_and_h450_bestv2/9VYK7oxcqhjd5LAH6ZFJ3XzOlID.jpg',
+                          height: 500,
+                          width: 300,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Padding(
+                          padding: const EdgeInsets.only(left: 40),
+                          child: Text(
+                            'Pedro',
+                            style: GoogleFonts.montserrat(
+                                textStyle: const TextStyle(
+                              fontSize: 30,
+                            )),
+                          )),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 40),
+                        child: Text(
+                          'Pascal',
+                          style: GoogleFonts.montserrat(
+                            textStyle: const TextStyle(
+                                fontSize: 18,
+                                color: Color.fromARGB(95, 73, 72, 72)),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
