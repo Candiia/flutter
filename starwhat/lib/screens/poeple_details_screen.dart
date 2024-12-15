3b46cfb5879c7df9ff1a0942eb2336ba777e6763
Candi Alcantarilla <alcantarilla.cacan24@triana.salesianos.edu>
2024-12-15 21:57:39 +0100

    detail starwars

--- starwhat/lib/screens/poeple_details_screen.dart
+++ starwhat/lib/screens/poeple_details_screen.dart
@@ -10,13 +10,49 @@ class PeopleDetail extends StatefulWidget {
 
 class _PeopleDetailState extends State<PeopleDetail> {
   @override
- Widget build(BuildContext context) {
+  Widget build(BuildContext context) {
+    final index = int.parse(widget.peopleItem.url!.split('/')[5]);
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.peopleItem.name!),
+      appBar: AppBar(
+        title: Text(widget.peopleItem.name!),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image(
+                image: NetworkImage(
+                    'https://starwars-visualguide.com/assets/img/characters/$index.jpg'),
+                width: 500,
+                height: 350,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Text(
+              "Nombre: ${widget.peopleItem.name}",
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "Altura: ${widget.peopleItem.height}",
+              style: const TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "Genero: ${widget.peopleItem.gender}",
+              style: const TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "Peso: ${widget.peopleItem.mass} Kg",
+              style: const TextStyle(fontSize: 18),
+            ),
+          ],
         ),
-        body: const Center(
-          child: CircularProgressIndicator(),
-        ));
+      ),
+    );
   }
 }
